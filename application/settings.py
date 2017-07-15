5319299c6aa022069f36f95e1a275e7e440a16c5
robotxt <ldimayuga@phixer.net>
2017-07-16 00:39:46 +0800

    adding corsheaders

--- application/settings.py
+++ application/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_celery_results',
+    'corsheaders',
     'www',
     'rest_framework',
     'rest_framework.authtoken',
